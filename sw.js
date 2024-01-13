d5c61e76a911a278221b06bb4a97af0408e74dd8
Bode87 <150076380+Bode87@users.noreply.github.com>
2024-01-13 19:45:55 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,33 @@
+const CACHE_NAME = 'game-of-life-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/images/icon.png'
+  // Fügen Sie hier weitere zu cachende Ressourcen hinzu
+];
+
 self.addEventListener('install', event => {
-    // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-        )
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
+
